806e20e59af5ef9a199fe5c299ba18ff16532398
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-17 13:56:17 -0400

    chore: Update tracking pixel service to include ID parameter and make the tracking a function

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
-import outdent from "outdent";
 
 const transparentPngBuffer = Buffer.from(
     "89504e470d0a1a0a0000000d49484452000000010000000108060000001f15c489000000017352474200aece1ce90000000b494441541857636000020000050001aad5c8510000000049454e44ae426082",
@@ -25,6 +24,16 @@ type IPInfo = {
     as: string;
 };
 
+type TrackedData = {
+    id: string | undefined;
+    type: string;
+    host: string;
+    pathname: string;
+    user_agent: string;
+    ip: string;
+    ip_info: IPInfo;
+};
+
 const app = new Elysia();
 app.use(
     cors({
@@ -33,41 +42,59 @@ app.use(
     }),
 );
 
-app.get("/", async () => {
-    return new Response(
-        `
+async function track(request: Request, id?: string) {
+    const referrer = request.headers.get("referer");
+    const { host, pathname } = referrer
+        ? new URL(referrer)
+        : {
+            host: "",
+            pathname: "",
+        };
+
+    const type = request.method;
+    const user_agent = request.headers.get("user-agent") || "";
+    const ip_list = request.headers.get("x-forwarded-for")?.split(",");
+    const ip = ip_list?.at(0)?.trim() || "";
+    const ip_info = await fetch(`http://ip-api.com/json/${ip}`);
+    const ip_info_json = (await ip_info.json()) as IPInfo;
+
+    const trackedData: TrackedData = {
+        id,
+        type,
+        host,
+        pathname,
+        user_agent,
+        ip,
+        ip_info: ip_info_json,
+    };
+
+    console.log(trackedData);
+}
+
+app
+    .get("/", async ({ request }) => {
+        track(request);
+
+        return new Response(
+            `
+            <!DOCTYPE html>
             <html>
                 <body>
-                    <h1>🦊 Elysia</h1>
+                    <h1>&#x1F98A; Elysia</h1>
                     <p>
-                        Elysia is a simple image server that returns a transparent PNG image.
+                        Welcome to Elysia! This is a simple tracking pixel service.
                     </p>
                 </body>
             </html>`,
-        {
-            headers: {
-                "content-type": "text/html",
+            {
+                headers: {
+                    "content-type": "text/html",
+                },
             },
-        },
-    );
-})
-    .get("/:id", async ({ params: { id }, query: { type }, request }) => {
-        const referrer = request.headers.get("referer");
-        const { host, pathname } = referrer
-            ? new URL(referrer)
-            : {
-                host: "",
-                pathname: "",
-            };
-
-        let result;
-        const user_agent = request.headers.get("user-agent");
-        const ip_list = request.headers.get("x-forwarded-for")?.split(",");
-        const ip = ip_list?.at(0)?.trim() || "";
-        const ip_info = await fetch(`http://ip-api.com/json/${ip}`);
-        const ip_info_json = (await ip_info.json()) as IPInfo;
-
-        console.log(ip_info_json);
+        );
+    })
+    .get("/id/:id", async ({ params: { id }, request }) => {
+        track(request, id);
 
         return new Response(transparentPngBuffer, {
             headers: {
@@ -78,5 +105,5 @@ app.get("/", async () => {
     .listen(4000);
 
 console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+    `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
